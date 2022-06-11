7a9fc63716521a00e3fe7a67e7ab37822cd0f511
David Puerto <dsp5502@gmail.com>
2022-06-10 19:09:48 -0500

    proyecto cotizador

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -1,11 +1,19 @@
+import { useCallback, useMemo, useRef } from 'react';
 import { MARCAS, PLANES } from '../constants';
 import useCotizador from '../hooks/useCotizador';
 
 const Resultado = () => {
   const { resultado, datos } = useCotizador();
   const { marca, plan, year } = datos;
-  const [nombreMarca] = MARCAS.filter((m) => m.id === Number(marca));
-  const [nombrePlan] = PLANES.filter((p) => p.id === Number(plan));
+  const yearRef = useRef(year);
+  const [nombreMarca] = useCallback(
+    MARCAS.filter((m) => m.id === Number(marca)),
+    [resultado]
+  );
+  const [nombrePlan] = useMemo(
+    () => PLANES.filter((p) => p.id === Number(plan)),
+    [resultado]
+  );
   if (resultado === 0) return null;
   return (
     <div className='bg-gray-100 text-center mt-5 p-5 shadow'>
@@ -20,7 +28,7 @@ const Resultado = () => {
       </p>
       <p className='my-2'>
         <span className='font-bold'>Año del Auto: </span>
-        {year}
+        {yearRef.currentuseMemo}
       </p>
       <p className='my-2 text-2xl'>
         <span className='font-bold'>Total Cotización: </span>
